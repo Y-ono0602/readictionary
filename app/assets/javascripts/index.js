5b9dc735848118aea0a10148f459924d3c873497
Y-ono0602 <ono.yuuki.ono@gmail.com>
2019-10-25 10:35:08 +0900

    1025

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -9,16 +9,24 @@ $(function() {
         <p class="top-info__right__title">${item.volumeInfo.title}</p>
         <p class="top-info__right__author">著者:${item.volumeInfo.authors}</p>
         <p class="top-info__right__description">${item.volumeInfo.description}</p>
-        <a href="https://www.amazon.co.jp/s?k=${item.volumeInfo.title}&i=stripbooks&__mk_ja_JP=カタカナ&ref=nb_sb_noss" target="_blank">
-        <button class="top-info__right__btn">
-        amazonで購入する</button></a>
         </div>
+      </div>
+      <div class="bottom-info">
+      <div class="bottom-info__title">各サイトで購入する</div>
+      <ul class="bottom-info__title__link">
+        <li><a href="https://www.amazon.co.jp/s?k=${item.volumeInfo.title}&i=stripbooks&__mk_ja_JP=カタカナ&ref=nb_sb_noss" target="_blank"><button class="bottom-info__btn amazon">amazonで購入する</button></a></li>
+        <li><a href="https://books.rakuten.co.jp/search?sitem=${item.volumeInfo.title}&g=001&l-id=pc-search-box&x=0&y=0
+        " target="_blank"><button class="bottom-info__btn rakuten">楽天ブックスで購入する</button></a></li>
+        <li><a href="https://honto.jp/netstore/search_10${item.volumeInfo.title}.html?srchf=1&tbty=0
+        " target="_blank"><button class="bottom-info__btn honto">hontoで購入する</button></a></li>
+        <li><a href="https://www.kinokuniya.co.jp/disp/CSfDispListPage_001.jsp?qs=true&ptk=01&q=${item.volumeInfo.title}
+        " target="_blank"><button class="bottom-info__btn knkny">紀伊國屋書店で購入する</button></a></li>
+      </ul>
       </div>
     </div>
     `
     return html;
   }
-        // <button class="top-info__right__btn" type="button">詳細をみる</button>
       //   <div class="bottom-info">
       //   <div class="bottom-info__title">概要</div>
       //   <div class="bottom-info__description">${item.volumeInfo.description}</div>
@@ -45,7 +53,8 @@ $(function() {
   });
 });
 
-$(document).on('submit', function(){
+$('#search').on('submit', function(e){
+  e.preventDefault();
   const Target = $('.search-results').offset();
   var speed = 1500;
   $('body,html').animate({scrollTop:Target.top}, speed, 'swing');
